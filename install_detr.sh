c4c6d67e0c040e182f927180fa83c5082fc4db4f
cyoukaikai <cyoukaikai@gmail.com>
2022-12-10 21:33:32 +0900

    update

--- install_detr.sh
+++ install_detr.sh
@@ -12,40 +12,8 @@ mkdir DN-DETR
 mv DN-DETR.zip DN-DETR
 cd DN-DETR
 unzip DN-DETR.zip 
+mv DN-DETR/* .
 
+chmod +x *.sh
+./install_sd.sh
 
-############################################
-cd smrc
-pip install -r requirements.txt
-
-cd ../
-pip install -r requirements.txt
-
-
-pip install einops
-pip install -U openmim
-mim install mmcv-full
-pip install mmdet
-
-pip install wandb
-
-touch ~/.tmux.conf
-echo "setw -g mouse on" >> ~/.tmux.conf
-tmux source-file ~/.tmux.conf
-tmux new -s train
-
-
-cd models/dn_dab_deformable_detr/ops
-python setup.py build install
-# python test.py
-cd ../../..
-
-############################################
-
-chmod +x coco.sh
-
-
-echo 8b14dd204de425f8a0c700ab58d00ec9ce60db4b
-# sftp://20.172.254.9/
-#  ssh -i ~/sddetr_key.pem kaikai@20.172.254.9
-wandb login
