1661bba6262d0cc7e8ee632415162da9a133d243
cyoukaikai <cyoukaikai@gmail.com>
2022-12-16 11:43:20 +0900

    update

--- install.sh
+++ install.sh
@@ -7,11 +7,18 @@ git clone https://github.com/cyoukaikai/SDDETR.git
 cd SDDETR
 chmod +x *.sh
 
-# mv pip.conf 
 
+mv pip.conf ~/.pip/pip.conf
+mv watch_gpu.sh ../.
+mv coco.sh ../.
 
 ./install_sd.sh
+./install_yun_coco.sh
 
+# MOVE the 
+mv SDDETR/DN-DETR ../DN-DETR
+
+cd DN-DETR
 
 echo 8b14dd204de425f8a0c700ab58d00ec9ce60db4b
 # tmux new -s train
