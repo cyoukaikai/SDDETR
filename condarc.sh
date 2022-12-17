032e58829b187c89edaf09087a8d5e720f1b13d8
cyoukaikai <cyoukaikai@gmail.com>
2022-12-17 10:52:51 +0900

    update

---
+++ condarc.sh
@@ -0,0 +1,15 @@
+channels:
+  - defaults
+show_channel_urls: true
+default_channels:
+  - http://mirrors.blockelite.cn:15018/anaconda/pkgs/main
+  - http://mirrors.blockelite.cn:15018/anaconda/pkgs/r
+  - http://mirrors.blockelite.cn:15018/anaconda/pkgs/msys2
+custom_channels:
+  conda-forge: http://mirrors.blockelite.cn:15018/anaconda/cloud
+  msys2: http://mirrors.blockelite.cn:15018/anaconda/cloud
+  bioconda: http://mirrors.blockelite.cn:15018/anaconda/cloud
+  menpo: http://mirrors.blockelite.cn:15018/anaconda/cloud
+  pytorch: http://mirrors.blockelite.cn:15018/anaconda/cloud
+  pytorch-lts: http://mirrors.blockelite.cn:15018/anaconda/cloud
+  simpleitk: http://mirrors.blockelite.cn:15018/anaconda/cloud
--- install.sh
+++ install.sh
@@ -3,23 +3,39 @@
 # export PATH=/usr/local/cuda/bin${PATH:+:${PATH}}
 # export LD_LIBRARY_PATH=/usr/local/cuda/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
 
-echo "export PATH=/usr/local/cuda/bin${PATH:+:${PATH}}" >> ~/.bashrc
-echo "export LD_LIBRARY_PATH=/usr/local/cuda/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}" >> ~/.bashrc
+#echo "export PATH=/usr/local/cuda/bin${PATH:+:${PATH}}" >> ~/.bashrc
+#echo "export LD_LIBRARY_PATH=/usr/local/cuda/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}" >> ~/.bashrc
 
 git clone https://github.com/cyoukaikai/SDDETR.git
 
 cd SDDETR
+
+mv pip.conf ~/.pip/pip.conf
+mv condarc.sh ~/.condarc
+
+
+
 chmod +x *.sh
 
 
-mv pip.conf ~/.pip/pip.conf
+
+
+conda create -n detr python=3.7 -y
+echo "conda activate detr" >> ~/.bashrc
+conda activate detr
+
+
+
+
 mv watch_gpu.sh ../.
 mv coco.sh ../.
 
 ./install_sd.sh
 ./install_yun_coco.sh
 
+pip install opencv-python-headless==4.5.2.52
 # MOVE the 
+cd ../
 mv SDDETR/DN-DETR ../DN-DETR
 
 cd DN-DETR
